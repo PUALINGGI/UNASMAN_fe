3eab97b1210a13a42b3b03edc4644a1a7828c553
Leonardo Pualinggi <leonardo.pualingi@gmail.com>
2022-07-19 19:57:57 +0800

    kesalahan #3

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -4,6 +4,7 @@ export default function Input(props) {
     return (
         <input type={props.type||"text"}
             min={props?.min??""} max={props?.max??""}
+            value={props?.value??""}
             placeholder={props.placeholder||""}
             className={`input font-bold bg-gray-500 px-5 md:px-10 border-transparent ${true&&props.classStatus} w-full max-w-full lg:max-w-lg`}
             onChange={true&&props.onChange}
