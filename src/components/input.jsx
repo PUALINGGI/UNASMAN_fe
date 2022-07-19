927b5dc73f09e340d5c9bc19aaba9a21692756e4
Leonardo Pualinggi <leonardo.pualingi@gmail.com>
2022-07-19 19:46:04 +0800

    Sedikit kesalahan

--- src/components/input.jsx
+++ src/components/input.jsx
@@ -3,6 +3,7 @@ import React from "react";
 export default function Input(props) {
     return (
         <input type={props.type||"text"}
+            min={props?.min??""} max={props?.max??""}
             placeholder={props.placeholder||""}
             className={`input font-bold bg-gray-500 px-5 md:px-10 border-transparent ${true&&props.classStatus} w-full max-w-full lg:max-w-lg`}
             onChange={true&&props.onChange}
