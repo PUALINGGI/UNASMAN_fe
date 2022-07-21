8b500f7ca6d4cd1272145f8fd7e7f0645a523949
Leonardo Pualinggi <leonardo.pualingi@gmail.com>
2022-07-21 18:20:01 +0800

    beberapa perubahan

--- src/components/modal.jsx
+++ src/components/modal.jsx
@@ -5,7 +5,7 @@ const MyModal = forwardRef((props, ref) => (
             <input ref={ref} type="checkbox" id="my-modal-3" className="modal-toggle" defaultChecked={props?.show??false} />
             <div className="modal">
                 <div className="modal-box relative">
-                    <button onClick={()=>{ document.querySelector("#my-modal-3").checked = false; }} className="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
+                    <button type="button" onClick={()=>{ document.querySelector("#my-modal-3").checked = false; }} className="btn btn-sm btn-circle absolute right-2 top-2">✕</button>
                     <h3 className="text-lg font-bold">{ props.header }</h3>
                     <p className="py-4">{ props.message }</p>
                 </div>
